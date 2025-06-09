c215be29806e0e2d2e9f69bdf777163f6e17a598
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-06-09 13:18:54 +0300

    Update MyForm.cpp

--- MyForm.cpp
+++ MyForm.cpp
@@ -1,10 +1,11 @@
 #include "MyForm.h"
+
 #include <Windows.h>
 using namespace TESTAGP;
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
-{
-    Application::EnableVisualStyles();
-    Application::SetCompatibleTextRenderingDefault(false);
-    Application::Run(gcnew MyForm);
-    return 0;
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
+                    _In_ LPWSTR lpCmdLine, _In_ int nShowCmd) {
+  Application::EnableVisualStyles();
+  Application::SetCompatibleTextRenderingDefault(false);
+  Application::Run(gcnew MyForm);
+  return 0;
 }
